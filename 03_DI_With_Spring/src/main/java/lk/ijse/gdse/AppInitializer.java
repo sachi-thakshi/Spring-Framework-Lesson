46f541ac38703dfdbbbaea10d02d28100820b11d
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2025-06-16 11:31:18 +0530

    Dependency Injection

--- 03_DI_With_Spring/src/main/java/lk/ijse/gdse/AppInitializer.java
+++ 03_DI_With_Spring/src/main/java/lk/ijse/gdse/AppInitializer.java
@@ -1,6 +1,18 @@
 package lk.ijse.gdse;
 
+import lk.ijse.gdse.bean.SpringBean;
+import lk.ijse.gdse.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppInitializer {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+
+        SpringBean bean = context.getBean(SpringBean.class);
+        System.out.println("BeanID : " + bean);
+
+        context.registerShutdownHook();
     }
 }
