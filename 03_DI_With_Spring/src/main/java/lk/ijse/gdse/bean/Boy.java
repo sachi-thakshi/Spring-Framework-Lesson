80fd6277cdd003fb129d8deb754d7155dec3903c
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2025-06-16 11:32:04 +0530

    Dependency Injection

---
+++ 03_DI_With_Spring/src/main/java/lk/ijse/gdse/bean/Boy.java
@@ -0,0 +1,34 @@
+package lk.ijse.gdse.bean;
+
+public class Boy {
+    // -------------------->>> 01. property injection
+//    Girl girl = new Girl();
+
+//    public void chatWithGirl() {
+//        girl.chat();
+//    }
+
+    // -------------------->>> 02.setter method through injection
+//    Girl girl;
+//    public Boy(Girl girl) {
+//        this.girl = girl;
+//    }
+//    public void chatWithGirl(){
+//        Boy boy = new Boy(new Girl());
+//        girl.chat();
+//    }
+
+    // -------------------->>> 03.constructor through injection
+//    Girl girl;
+//    public void SetterMethod(Girl girl) {
+//        this.girl = girl;
+//    }
+//    public void chatWithGirl(){
+//        Boy boy = new Boy();
+//        boy.SetterMethod(new Girl());
+//        girl.chat();
+//    }
+
+    // -------------------->>> 04.interface through injection
+
+}
---
+++ 03_DI_With_Spring/src/main/java/lk/ijse/gdse/bean/Girl.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse.bean;
+
+import org.springframework.stereotype.Component;
+
+public class Girl {
+    public void chat(){
+        System.out.println("Chatting...");
+    }
+}
