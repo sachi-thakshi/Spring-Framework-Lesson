4aa9b191c88d4c329a42844a14b28c0164dfe62b
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2025-06-30 10:19:59 +0530

    Spring web MVC - Dispatcher Servlet

--- 08_Spring_WebMVC/src/main/java/lk/ijse/gdse/config/WebAppConfig.java
+++ 08_Spring_WebMVC/src/main/java/lk/ijse/gdse/config/WebAppConfig.java
@@ -1,11 +1,30 @@
 package lk.ijse.gdse.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan({"lk.ijse.gdse.bean", "lk.ijse.gdse.controller"})
 @EnableWebMvc
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/views/");
+        viewResolver.setSuffix(".html");
+        viewResolver.setOrder(2);
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/views/**").addResourceLocations("/views/");
+    }
 }
--- 08_Spring_WebMVC/src/main/java/lk/ijse/gdse/controller/HelloController.java
+++ 08_Spring_WebMVC/src/main/java/lk/ijse/gdse/controller/HelloController.java
@@ -1,23 +1,31 @@
 package lk.ijse.gdse.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping("hello")
 public class HelloController {
     public HelloController() {
         System.out.println("HelloController Constructor");
     }
 
-    @GetMapping("one")
-    public String hello1() {
-        return "Hello World One";
-    }
+//    @GetMapping("one")
+//    public String hello1() {
+//        return "Hello World One";
+//    }
 
-    @GetMapping("two")
-    public String hello2() {
-        return "Hello World Two";
+//    @GetMapping("two")
+//    public String hello2() {
+//        return "Hello World Two";
+//    }
+
+      @GetMapping()
+      public String hello() {
+        return "index";
+//        return "Hello World Two";
     }
 }
